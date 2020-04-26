cbb36a7e5bcb0e0436d2b1f8c4589938b4828df8
Guilherme Limpo <guilhermelimpo@gmail.com>
2020-04-26 11:44:04 -0300

    Adicionar o docker do postgres

---
+++ Docker/init.sql
@@ -0,0 +1,7 @@
+CREATE USER docker;
+
+CREATE DATABASE gostack_desafio06;
+GRANT ALL PRIVILEGES ON DATABASE gostack_desafio06 TO docker;
+
+CREATE DATABASE gostack_desafio06_tests;
+GRANT ALL PRIVILEGES ON DATABASE gostack_desafio06_tests TO docker;
